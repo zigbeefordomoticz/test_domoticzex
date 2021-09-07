a83872162c87e643d7273390f70e942dcbf55b32
Patrick Pichon <patrick@pichon.me>
2021-09-07 14:47:50 +0200

    init file domoTools.py

---
+++ domoTools.py
@@ -0,0 +1,13 @@
+
+
+def get_widget_attributes( DeviceId, Unit, Attribute=None):
+    
+    return dict()
+
+def get_widget_unit():
+
+    return ( DeviceId, Unit)
+
+def update_widget_attributes( DeviceId, Unit, attributes_dict):
+
+
